ac5fd6a8e1a20d2c97b407141f0b1215c972da2e
Jonnas Cipriano <jonnascipriano@gmail.com>
2024-07-22 22:19:52 -0300

    tt

--- funcoes.js
+++ funcoes.js
@@ -315,3 +315,21 @@ function vcvotav(){
 }
 
 // SECTION 'VOCE VOTA?'**************************************************************
+
+
+
+let count = 1;
+document.getElementById('radio1').checked = true;
+
+setInterval(function(){
+    nextImage()
+}, 2000);
+
+function nextImage(){
+    count++;
+    if(count>4){
+        count = 1
+    }
+document.getElementById('radio'+count).checked = true;
+
+}
